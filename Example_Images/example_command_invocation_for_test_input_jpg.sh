83478c04ccb0f8f985eb588b06e945dbc9384084
Jacob Levernier <jlevern@upenn.edu>
2017-02-09 09:09:19 -0500

    Moved offsets to correct location in code, and added example invocation for the example image provided in the original repo.

---
+++ Example_Images/example_command_invocation_for_test_input_jpg.sh
@@ -0,0 +1,13 @@
+./bin/bookscan \
+--verbose \
+--offset-left-page-left-side 0.3 \
+--offset-left-page-right-side 0.3 \
+--offset-left-page-top-side 0.2 \
+--offset-left-page-bottom-side -0.2 \
+--offset-right-page-left-side -0.4 \
+--offset-right-page-right-side -0.3 \
+--offset-right-page-top-side 0.25 \
+--offset-right-page-bottom-side -0.2 \
+-i bin/docs/test_input.jpg \
+~/Downloads/bookscan_output/test1.jpg \
+~/Downloads/bookscan_output/test2.jpg
--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ R"(Bookscan.
       
       --verbose     Show additional output, including the values of every option the program accepts.
       
-      -t --page-height=<page_height_argument>  Height of each page (in any metric) ('t' is for 'tall'). [default: 10.0]
+      -t --page-height=<page_height_argument>  Height of each page (in any metric) ('t' is for 'tall'). [default: 9.5]
       -w --page-width=<page_width_argument>  Width of each page (in any metric). [default: 6.0]
       --no-left-page  Only process right-side pages (Markers 0-3).
       --no-right-page  Only process left-side pages (Markers 4-7).
@@ -73,15 +73,15 @@ R"(Bookscan.
       <output_image_one>  The output image. Needs to have an image-like file extension (e.g., ".jpg", ".JPG", ".png", ".tif", ".tiff").
       <output_image_two>  If relevant, the second output image (see <output_image_one> above).
       
-      --offset-left-page-left-side=<offset_left_page_left_side>  Page offset, in the same units as page height and width. [default: 0.0]
-	  --offset-left-page-right-side=<offset_left_page_right_side>  Page offset, in the same units as page height and width. [default: 0.0]
-	  --offset-left-page-top-side=<offset_left_page_top_side>  Page offset, in the same units as page height and width. [default: 0.0]
-	  --offset-left-page-bottom-side=<offset_left_page_bottom_side>  Page offset, in the same units as page height and width. [default: 0.0]
+      --offset-left-page-left-side=<offset_left_page_left_side>  Page offset, in the same units as page height and width. [default: 0.00]
+	  --offset-left-page-right-side=<offset_left_page_right_side>  Page offset, in the same units as page height and width. [default: 0.00]
+	  --offset-left-page-top-side=<offset_left_page_top_side>  Page offset, in the same units as page height and width. [default: 0.00]
+	  --offset-left-page-bottom-side=<offset_left_page_bottom_side>  Page offset, in the same units as page height and width. [default: 0.00]
 	  
-	  --offset-right-page-left-side=<offset_right_page_left_side>  Page offset, in the same units as page height and width. [default: 0.0]
-	  --offset-right-page-right-side=<offset_right_page_right_side>  Page offset, in the same units as page height and width. [default: 0.0]
-	  --offset-right-page-top-side=<offset_right_page_top_side>  Page offset, in the same units as page height and width. [default: 0.0]
-	  --offset-right-page-bottom-side=<offset_right_page_bottom_side>  Page offset, in the same units as page height and width. [default: 0.0]
+	  --offset-right-page-left-side=<offset_right_page_left_side>  Page offset, in the same units as page height and width. [default: 0.00]
+	  --offset-right-page-right-side=<offset_right_page_right_side>  Page offset, in the same units as page height and width. [default: 0.00]
+	  --offset-right-page-top-side=<offset_right_page_top_side>  Page offset, in the same units as page height and width. [default: 0.00]
+	  --offset-right-page-bottom-side=<offset_right_page_bottom_side>  Page offset, in the same units as page height and width. [default: 0.00]
       
     Debugging mode:
       Running the program without any arguments will open a webcam window for real-time glyph detection (for calibration). If a webcam is found, this will cause a window to open, showing output from the webcam. When the four "left page" glyphs (i.e., glyphs 0, 1, 2, and 3) are detected by the webcam, a new window will open showing the de-keystoned image that the four glyphs surround. Similarly, when the four "right page" glyphs (i.e., glyphs 4, 5, 6, and 7) are detected by the webcam, an additional new window will open, showing the de-keystoned image for those four glyphs. Throughout this process, debugging text will be given in the terminal window, including which glyphs are detected.
@@ -238,18 +238,18 @@ int main(int argc, const char** argv)
     // Configure left page. Glyph '0' is expected to be in the top left corner of the page. Glyph '1' is expected to be in the top right corner, etc. going clockwise.
     // These points are in the form cvPoint2D32f(horizontal_location, vertical_location) in whatever units page_width and page_height are (what matters is their relation to each other, and not as much the units themselves, although larger units will mean larger output images).
     std::map<int, CvPoint2D32f> left_dst_markers;
-    left_dst_markers[0] = cvPoint2D32f(0.00 + offset_left_page_left_side, 0.00 + offset_left_page_top_side);
-    left_dst_markers[1] = cvPoint2D32f(page_width + offset_left_page_right_side, 0.00 + offset_left_page_top_side);
-    left_dst_markers[2] = cvPoint2D32f(page_width + offset_left_page_left_side, page_height + offset_left_page_bottom_side);
-    left_dst_markers[3] = cvPoint2D32f(0.00 + offset_left_page_right_side, page_height + offset_left_page_bottom_side);
+    left_dst_markers[0] = cvPoint2D32f(0.00, 0.00);
+    left_dst_markers[1] = cvPoint2D32f(page_width, 0.00);
+    left_dst_markers[2] = cvPoint2D32f(page_width, page_height);
+    left_dst_markers[3] = cvPoint2D32f(0.00, page_height);
     
     // Configure right page. Similar to the left page, Glyph '4' is expected to be in the top left corner of the page, with each higher-numbered glyph in the next corner, going around the page clockwise.
     // See above in the "Configure left page" section re: the format of these points.
     std::map<int, CvPoint2D32f> right_dst_markers;
-    right_dst_markers[4] = cvPoint2D32f(0.00 + offset_right_page_left_side, 0.00 + offset_right_page_top_side);
-    right_dst_markers[5] = cvPoint2D32f(page_width + offset_right_page_right_side, 0.00 + offset_right_page_top_side);
-    right_dst_markers[6] = cvPoint2D32f(page_width + offset_right_page_left_side, page_height + offset_right_page_bottom_side);
-    right_dst_markers[7] = cvPoint2D32f(0.00 + offset_right_page_right_side, page_height + offset_right_page_bottom_side);
+    right_dst_markers[4] = cvPoint2D32f(0.00, 0.00);
+    right_dst_markers[5] = cvPoint2D32f(page_width, 0.00);
+    right_dst_markers[6] = cvPoint2D32f(page_width, page_height);
+    right_dst_markers[7] = cvPoint2D32f(0.00, page_height);
     
     // Define additional information about the pages:
     LayoutInfo left_layout;
@@ -262,15 +262,15 @@ int main(int argc, const char** argv)
     // To define an offset, just add or subtract from the numbers below (e.g., "page_width - 0.30").
     // These offsets should be on the same scale as the page width and height you pass to the program through the command line (e.g., inches) -- it doesn't matter which scale you use, as long as it's consistent (the important thing is the relative sizes of the numbers given to the program).
     
-    left_layout.page_left = 0;
-    left_layout.page_top = 0;
-    left_layout.page_right = page_width + 0.4;
-    left_layout.page_bottom = page_height - 0.05;
+    left_layout.page_left = 0 + offset_left_page_left_side;
+    left_layout.page_top = 0 + offset_left_page_top_side;
+    left_layout.page_right = page_width + offset_left_page_right_side;
+    left_layout.page_bottom = page_height + offset_left_page_bottom_side;
     
-    right_layout.page_left = 0;
-    right_layout.page_top = 0;
-    right_layout.page_right = page_width + 0.4;
-    right_layout.page_bottom = page_height - 0.05;
+    right_layout.page_left = 0 + offset_right_page_left_side;
+    right_layout.page_top = 0 + offset_right_page_top_side;
+    right_layout.page_right = page_width + offset_right_page_right_side;
+    right_layout.page_bottom = page_height + offset_right_page_bottom_side;
     
     // Process if an input image is supplied; otherwise, open a webcam for
     // debugging.
